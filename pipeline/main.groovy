28632f39261de2aa4e3d42c2f87840c51b721439
roms12 <50302773+roms12@users.noreply.github.com>
2021-06-06 12:23:38 +0200

    add images

--- pipeline/main.groovy
+++ pipeline/main.groovy
@@ -1,6 +1,36 @@
 def call() {
-  stage("discover") {
-    echo "Let Jenkins discover the repository"
+  def build_version = "unknown"
+
+  stage("version") {
+    echo "TODO: implement build versions"
+    
+    if (is_pull_request()) {
+      build_version = "PR" + env.CHANGE_ID + "." + env.BUILD_NUMBER
+    }
+    else {
+      build_version = env.BRANCH_NAME + "." + env.BUILD_NUMBER
+    }
+    echo "version is ${build_version}"
+  }
+
+  stage("images") {
+    def config = readYaml file: 'images/config.yml'
+    echo "TODO: implement multi arch"
+    for (image in config.images) {
+      for (arch in image.archs) {
+        if (arch == "armv7") {
+          echo "build ${image.name}/${arch} version ${build_version}"
+          sh """
+            docker build                                \
+              -t ${image.name}/${arch}:${build_version} \
+              --build-arg version="${build_version}"    \
+              --build-arg arch="${arch}"                \
+              -f images/Dockerfile.${image.name}        \
+              images/
+          """
+        }
+      }
+    }
   }
 }
 return this
